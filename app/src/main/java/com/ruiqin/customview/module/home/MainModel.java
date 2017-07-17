6a20b6b303a3c0cc55f203c2fb8ab07e762d914e
shenruiqin <shenruiqin@100cb.cn>
2017-07-17 20:51:42 +0800

    viewFiler

--- app/src/main/java/com/ruiqin/customview/module/home/MainModel.java
+++ app/src/main/java/com/ruiqin/customview/module/home/MainModel.java
@@ -3,6 +3,7 @@ package com.ruiqin.customview.module.home;
 import com.ruiqin.customview.module.dialog.DialogActivity;
 import com.ruiqin.customview.module.elevation.ElevationActivity;
 import com.ruiqin.customview.module.home.bean.MainRecyclerData;
+import com.ruiqin.customview.module.textview.TextViewActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +19,7 @@ public class MainModel implements MainContract.Model {
         List<MainRecyclerData> recyclerDataList = new ArrayList<>();
         recyclerDataList.add(new MainRecyclerData("AlertDialog", DialogActivity.class));
         recyclerDataList.add(new MainRecyclerData("FrameLayout", ElevationActivity.class));
+        recyclerDataList.add(new MainRecyclerData("TextViewActivity", TextViewActivity.class));
         return recyclerDataList;
     }
 }
